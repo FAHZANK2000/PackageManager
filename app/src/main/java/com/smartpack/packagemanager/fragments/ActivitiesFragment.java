567ee9fc90298562f82c3cd4f137219406944a8a
sunilpaulmathew <sunil.kde@gmail.com>
2025-10-23 23:36:46 +0200

    Slightly modified Activities recyclerview layout
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- app/src/main/java/com/smartpack/packagemanager/fragments/ActivitiesFragment.java
+++ app/src/main/java/com/smartpack/packagemanager/fragments/ActivitiesFragment.java
@@ -20,6 +20,7 @@ import android.widget.ProgressBar;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -72,6 +73,7 @@ public class ActivitiesFragment extends Fragment {
                 mRecyclerView = mRootView.findViewById(R.id.recycler_view);
                 mProgressBar.setVisibility(VISIBLE);
                 mRecyclerView.setLayoutManager(new LinearLayoutManager(requireActivity()));
+                mRecyclerView.addItemDecoration(new DividerItemDecoration(requireActivity(), DividerItemDecoration.VERTICAL));
             }
 
             @Override
